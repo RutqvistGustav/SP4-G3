fbdd13d3c1c9079f4e94f06199d79069849b4926
TGA-RobinKrokfors <71003044+TGA-RobinKrokfors@users.noreply.github.com>
2021-03-10 16:14:31 +0100

    Work on simple sprite sheet animator

---
+++ Source/Game/SpriteSheetAnimation.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#include <unordered_map>
+#include <vector>
+
+namespace Tga2D
+{
+	class CTexture;
+}
+
+class JsonManager;
+class SpriteWrapper;
+
+class SpriteSheetAnimation
+{
+public:
+
+	SpriteSheetAnimation(const JsonManager* aJsonManager, const std::string& anAnimationPath);
+	SpriteSheetAnimation(const JsonManager* aJsonManager, const char* anAnimationPath);
+
+	void Update(float aDeltaTime);
+
+	void ApplyToSprite(const std::shared_ptr<SpriteWrapper>& aSprite);
+
+	void SetState(const std::string& aState);
+	const std::string& GetState() const;
+
+	float GetDuration() const;
+
+	bool InState() const;
+
+	inline void SetIsLooping(bool anIsLooping) { myIsLooping = anIsLooping; }
+	inline bool IsLooping() const { return myIsLooping; }
+
+private:
+
+	struct AnimationFrame
+	{
+		float myX;
+		float myY;
+		float myWidth;
+		float myHeight;
+	};
+
+	struct AnimationState
+	{
+		float myInvFps{};
+
+		Tga2D::CTexture* myTexture{};
+		std::vector<AnimationFrame> myFrames;
+	};
+
+	const AnimationState& GetCurrentState() const;
+
+	void Load(const JsonManager* aJsonManager, const std::string& anAnimationPath);
+	Tga2D::CTexture* TryLoadTexture(const std::string& aPath) const;
+
+private:
+
+	float myTime{};
+	bool myIsLooping{};
+
+	std::string myActiveState;
+	std::unordered_map<std::string, AnimationState> myStates;
+
+};
