84398327d2d6de68d16c32307f51677db0423eeb
PerryIvic <71003066+PerryIvic@users.noreply.github.com>
2021-03-11 15:42:10 +0100

    Changed Player variables

--- Source/Game/Player.h
+++ Source/Game/Player.h
@@ -42,15 +42,15 @@ private:
 
     float mySpeed;
     float myMaxSpeed;
-    float myReduceMovementSpeed;
-    float myStopAtVelocity;
+    float myStopAtVelocity; 
     float myGravity;
+    double myReduceMovementSpeed;
 
     // Jump
     bool myIsJumping = false;
     bool myHasRemovedNegativeVel = false;
-    unsigned int myJumpCharges;
-    unsigned int myJumpChargeReset;
+    int myJumpCharges;
+    int myJumpChargeReset;
     float myJumpStrength;
     float myJumpDuration;
     float myJumpDurationReset;
@@ -69,5 +69,8 @@ private:
 
     // Constructor
     void InitVariables(nlohmann::json someData);
+
+    // Tools
+    void ImGui();
 };
 
