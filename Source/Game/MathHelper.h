f59dc8e7d9dcd64494bfd4b502a7f0a601354226
TGA-RobinKrokfors <71003044+TGA-RobinKrokfors@users.noreply.github.com>
2021-03-16 18:06:54 +0100

    Fix division by zero in MathHelper::MoveTowards

--- Source/Game/MathHelper.h
+++ Source/Game/MathHelper.h
@@ -28,6 +28,11 @@ namespace MathHelper
 		CU::Vector2<float> toTarget = aTarget - aValue;
 		float length = toTarget.Length();
 
+		if (length <= 0.0f)
+		{
+			return aTarget;
+		}
+
 		toTarget /= length;
 
 		if (length > aMaxDelta)
