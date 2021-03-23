1964f10ed7f05260550bba5518ebac9c36b51620
PuEkstrand <pu.ekstrand@studerande.thegameassembly.se>
2021-03-23 16:46:10 +0100

    Created som basic logic for HealthPickup
    :D

--- Source/Game/HealthPickup.cpp
+++ Source/Game/HealthPickup.cpp
@@ -1,2 +1,19 @@
 #include "stdafx.h"
 #include "HealthPickup.h"
+#include "Collectable.h"
+#include "Player.h"
+#include "Scene.h"
+#include "GlobalServiceProvider.h"
+#include "JsonManager.h"
+
+HealthPickup::HealthPickup(Scene* aScene)
+	: Collectable(aScene)
+{
+	myHealingValue = 1;
+	//InitWithJson(GetScene()->GetGlobalServiceProvider()->GetJsonManager()->GetData());
+}
+
+void HealthPickup::OnCollect(Player* aPlayer)
+{
+	//TODO - Give Player The Healing Value;
+}
--- Source/Game/HealthPickup.h
+++ Source/Game/HealthPickup.h
@@ -1,5 +1,13 @@
 #pragma once
-class HealthPickup
+#include "Collectable.h"
+class HealthPickup : public Collectable
 {
+public:
+	HealthPickup(Scene* aScene);
+
+	virtual void OnCollect(Player* aPlayer) override;
+
+private:
+	int myHealingValue;
 };
 
