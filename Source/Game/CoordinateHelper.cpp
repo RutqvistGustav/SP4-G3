32618f24334e86875808b67a23d9114aeadacdc8
Gustav Rutqvist <rutqvistgustav@gmail.com>
2021-03-29 19:22:15 +0200

    mousebug fix

--- Source/Game/CoordinateHelper.cpp
+++ Source/Game/CoordinateHelper.cpp
@@ -1,4 +1,5 @@
 #include "stdafx.h"
 #include "CoordinateHelper.h"
+#include "Metrics.h"
 
-const CU::Vector2<float> CoordinateHelper::ourReferenceSize = CU::Vector2<float>(1920.f, 1080.f);
+const CU::Vector2<float> CoordinateHelper::ourReferenceSize = CU::Vector2<float>(Metrics::GetReferenceSize());
