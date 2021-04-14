ead5d3dd53bdceaf2c8e3e7ebc1671c07fe0319f
PerryIvic <71003066+PerryIvic@users.noreply.github.com>
2021-04-14 12:03:35 +0200

    Working on Key collectable as goal to Level 2

--- Source/Game/Key.cpp
+++ Source/Game/Key.cpp
@@ -2,6 +2,10 @@
 #include "Key.h"
 #include "GlobalServiceProvider.h"
 #include "JsonManager.h"
+#include "LevelManagerProxy.h"
+
+#include "DialogueBox.h"
+#include "SpriteSheetAnimation.h"
 
 // TODO: REMOVE
 #include <iostream>
@@ -14,11 +18,43 @@ Key::Key(Scene* aScene, PowerUpType aPowerupType)
 
 void Key::InitWithJson(const JsonData& someProperties)
 {
-	mySprite = std::make_shared<SpriteWrapper>(someProperties.at("SpritePath"));
+	Init();
+
+	myDialogBox = std::make_unique<DialogueBox>(GetScene(), true);
+	myDialogBox->Init(stringID);
+
+	mySprite = std::make_shared<SpriteWrapper>();
+	myAnimation = std::make_unique<SpriteSheetAnimation>(myScene->GetGlobalServiceProvider()->GetJsonManager(), "Animation/TestAnimation.json");
+
+	myAnimation->SetState("idle");
+	myAnimation->SetIsLooping(true);
+
+	myAnimation->ApplyToSprite(mySprite);
+	SetTriggerSize(mySprite->GetSize());
 }
 
 void Key::OnCollect(Player* /*aPlayer*/)
 {
 	// TODO Activate dialogue box
-	// Then start transition to nextlevel.
+}
+
+void Key::Update(const float aDeltaTime, UpdateContext& anUpdateContext)
+{
+	TriggerVolume::Update(aDeltaTime, anUpdateContext);
+
+	myDialogBox->Update(aDeltaTime, anUpdateContext);
+
+	myAnimation->Update(aDeltaTime);
+	myAnimation->ApplyToSprite(mySprite);
+
+	if (myDialogBox->GetDeleteThisFrame() == true) 
+	{
+		GetScene()->GetLevelManagerProxy()->TransitionNextLevel();
+	}
+}
+
+void Key::Render(RenderQueue* const aRenderQueue, RenderContext& aRenderContext)
+{
+	GameObject::Render(aRenderQueue, aRenderContext);
+	myDialogBox->Render(aRenderQueue, aRenderContext);
 }
