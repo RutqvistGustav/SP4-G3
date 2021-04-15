48440f61a92e73ac286b96f22372944768e0d65a
Vilgot Larsson <vilgot.larsson@studerande.thegameassembly.se>
2021-04-15 14:37:29 +0200

    städade upp i MAPS foldern.

--- Bin/Maps/Tilesets/TS.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="2021.02.15" name="TS" tilewidth="64" tileheight="64" tilecount="1024" columns="32">
- <image source="../../Sprites/spriteSheets/spriteSheet_01.png" width="2048" height="2048"/>
-</tileset>
